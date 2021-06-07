969b70ab54be2b89989d2254500307d923bca3dd
Edwin <el5091@g.rit.edu>
2021-06-07 19:07:49 -0400

    -able to display both error and latitude when given access or not -connected index.js to SeasonDIsplay.js -able to use latitutde and month info to display proper text and icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+	if (month > 2 && month < 9) {
 
-const SeasonDisplay = () => {
-	return <div> Season Display </div>
+		//If lat is > 0, return summer, otherwise winter
+		return lat > 0 ? 'summer' : 'winter';
+	}
+
+	else {
+		return lat > 0 ? 'winter' : 'summer';
+	}
 }
+
+
+const SeasonDisplay = props => {
+	const season = getSeason(props.lat, new Date().getMonth());
+	const text = season == 'winter' ? 'Brr, it is chilly' : 'Lets hit the beach';
+	const icon = season == 'winter' ? 'snowflake' : 'sun' ;
+
+	return (
+		<div>
+			<i className={`${icon} icon`} />
+			<h1> {text} </h1>
+			<i className={`${icon} icon`} />
+		</div>
+	);
+};
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -7,33 +7,34 @@ import SeasonDisplay from './SeasonDisplay';
    AKA subclassing React.Component */
 class App extends React.Component {
 
-    // Not required by React, automatically and instantly called before anything else
-    constructor(props) {
+    /* the constructor part is 100% equivalent to just this:  */
+    state = { lat: null, errorMessage: '' };
 
-        /* To make sure that parents or Reacts' components constructur function gets called
-           Reference to the parent's constructor function 
-           ALWAYS MUST BE DONE */
-        super(props);
-
-        // Eventually contain some diff pieces of data and properties that are important and relevant
-        // ONLY DO THIS WHEN WE INITIALIZE THE STATE !!!
-        this.state = { lat: null };
-
+    componentDidMount() {
         // Detects the user's location -- for this project only, we care only for latitude
         window.navigator.geolocation.getCurrentPosition(
-            position => {
 
-                // to update state, we use setState!!!
-                this.setState({ lat: position.coords.latitude });
-            },
-            err => console.log(err)
+            // to update state, we use setState!!!
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
 
     // Reacts says we HAVE to define render!!!
     // This is called a lot so we do NOT want to put fetching code here
     render() {
-        return <div> Latitude: {this.state.lat} </div>;
+
+        // Conditional rendering (if statements) 
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div> Error: {this.state.errorMessage} </div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <div> Loading...  </div>;
+
     }
 }
 
