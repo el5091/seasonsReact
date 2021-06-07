fb4a553ebe1597c85ed1710a40c6b3cf181cd609
Edwin <el5091@g.rit.edu>
2021-06-07 19:36:34 -0400

    -did some css for the seasons page -added a loading page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SeasonDisplay from './SeasonDisplay';
+import Loading from './Loading';
 
 /* Allows us to pull a ton of built in functionality
    AKA subclassing React.Component */
@@ -33,7 +34,7 @@ class App extends React.Component {
             return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div> Loading...  </div>;
+        return <Loading message="Please accept location request..."/>;
 
     }
 }
